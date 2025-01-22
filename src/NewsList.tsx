eeeef44d01a6b7171fa12e90e34ab3d8cc3795ed
ZheNing Hu <adlternative@gmail.com>
2025-01-22 15:22:52 +0800

    refactor(newslist): use grid to control news-list-item
    Signed-off-by: ZheNing Hu <adlternative@gmail.com>

--- src/NewsList.tsx
+++ src/NewsList.tsx
@@ -26,47 +26,49 @@ const NewsList: React.FC<Props> = ({ news, currentPage }) => {
       <ul className="news-list">
         {news.map((item, index) => (
           <li key={item.id} className="news-list-item">
-            <span className="news-list-number">
-              {(currentPage - 1) * 30 + index + 1}.
-            </span>
-            <button
-              className="vote-button"
-              onClick={() => handleVote(item.id)}
-              aria-label={`Vote for ${item.title}`}
-            >
-              <img src="triangle.svg" alt="Vote" className="vote-triangle" />
-            </button>
-            <div className="news-content-with-meta">
-              <div className="news-content">
-                <a
-                  href={item.url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="news-list-title-link"
-                >
-                  <h2 className="news-list-item-title">{item.title}</h2>
-                </a>
-                <a
-                  className="news-list-item-link"
-                  href={item.url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {`(${item.url})`}
-                </a>
-              </div>
-              <div className="news-list-item-meta">
-                <span> {item.pointsCount} points </span>
-                {/* <span> by {item.author.name} </span> */}
-                <a href={`/users?name=${item.author.name}`}>{item.author.name}</a>
-                <span> {relativeTimeFromISOString(item.updatedAt)} </span>
-                <a href="#" className="with-vertical-bar">hide</a>
-                <a href={`/item?id=${item.id}`}>
-                  {item.commentsCount == 0
-                    ? "discuss"
-                    : item.commentsCount + " comments"}
-                </a>
-              </div>
+            <div className="news-list-item-left-part">
+              <span className="news-list-number">
+                {(currentPage - 1) * 30 + index + 1}.
+              </span>
+              <button
+                className="vote-button"
+                onClick={() => handleVote(item.id)}
+                aria-label={`Vote for ${item.title}`}
+              >
+                <img src="triangle.svg" alt="Vote" className="vote-triangle" />
+              </button>
+            </div>
+            <div className="news-content">
+              <a
+                href={item.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="news-list-title-link"
+              >
+                <h2 className="news-list-item-title">{item.title}</h2>
+              </a>
+              <a
+                className="news-list-item-link"
+                href={item.url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {`(${item.url})`}
+              </a>
+            </div>
+            <div className="news-list-item-meta">
+              <span> {item.pointsCount} points </span>
+              {/* <span> by {item.author.name} </span> */}
+              <a href={`/users?name=${item.author.name}`}>{item.author.name}</a>
+              <span> {relativeTimeFromISOString(item.updatedAt)} </span>
+              <a href="#" className="with-vertical-bar">
+                hide
+              </a>
+              <a href={`/item?id=${item.id}`}>
+                {item.commentsCount == 0
+                  ? "discuss"
+                  : item.commentsCount + " comments"}
+              </a>
             </div>
           </li>
         ))}
