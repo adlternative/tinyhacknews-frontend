a5dd831f23e88c8d0ac1610bf03b12fd223a8df8
ZheNing Hu <adlternative@gmail.com>
2025-01-30 16:58:39 +0800

    chore(url): remove suffix slash
    Signed-off-by: ZheNing Hu <adlternative@gmail.com>

--- src/utils/uri.tsx
+++ src/utils/uri.tsx
@@ -1,7 +1,8 @@
-// 提供一个网页链接，去除 http:// 或 https:// 和 www. 前缀，去除后置参数
+// 提供一个网页链接，去除 http:// 或 https:// 和 www. 前缀，去除后置参数, 并取出 url 末尾的 / 符号
 
 function GetPureURI(uri: string): string {
-  return uri.replace(/^(https?:\/\/)?(www\.)?/, '').split('?')[0];
+  uri = uri.replace(/^(https?:\/\/)?(www\.)?/, '').split('?')[0];
+  return uri.replace(/\/$/, '');
 }
 
 export default GetPureURI;
